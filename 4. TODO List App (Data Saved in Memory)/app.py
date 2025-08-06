cf4069addacd5b50af8d8a2d517dd240e2397539
Moon-kazim <kazim.shaikh0@gmail.com>
2025-08-06 15:30:12 +0530

    adding requirement.txt file

--- 4. TODO List App (Data Saved in Memory)/app.py
+++ 4. TODO List App (Data Saved in Memory)/app.py
@@ -4,6 +4,19 @@ import csv
 
 app = Flask(__name__)
 
+'''
+Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
 # Dummy todo list
 todos = []
 
